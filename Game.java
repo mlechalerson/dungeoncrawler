6e4da2393e1bb60879b4bfe77fd5cd722da81af2
mlechalerson <166102397+mlechalerson@users.noreply.github.com>
2024-05-16 14:14:22 +0200

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Game extends JFrame {
+    public Game() {
+        setTitle("Dungeon Crawler");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 608, 720);
+    }
+
+    public generateMap(){
+        for (int )
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+        Game frame = new Game();
+        frame.setVisible(true);
+
+    }
+
+}
